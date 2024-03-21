9296c7fa9b7f17160ac975d46f60e0569d40cfb9
Liam5305 <liamrutherford10@gmail.com>
2024-03-21 17:15:41 +0000

    CSS Changes
    Made changes to CSS and implemented Material UI Buttons

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,11 +4,14 @@ import {
     Route,
     NavLink,
     HashRouter
-} from 'react-router-dom'
-
-
+} from 'react-router-dom';
 import HomePage from "./pages/Home";
+import PackagesPage from "./pages/Packages";
+import GalleryPage from "./pages/Gallery";
+import ContactPage from "./pages/Contact";
 
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
 
 function Main() {
     return (
@@ -17,23 +20,23 @@ function Main() {
                 <>
                     <div>
                         <div className="one3media-header">
-                            <h1>ONE3MEDIA</h1>
+                            <title>ONE3MEDIA</title>
                             <h3>Professional Film/Photo</h3>
-                            <ul className="nav">
-                                <li><NavLink to="/">Home</NavLink></li>
-                                <li><NavLink to="/">Packages</NavLink></li>
-                                <li><NavLink to="/">Gallery</NavLink></li>
-                                <li><NavLink to="/">Contact</NavLink></li>
-                                {/* Add more navs here */}
+                            <ul id="one3media-nav">
+                            <Stack direction="row" spacing={2}>
+                                <Button><li><NavLink to="/">Home</NavLink></li></Button>
+                                <Button><Button> <li><NavLink to="/Packages">Packages</NavLink></li></Button>
+                                <Button><li><NavLink to="/Gallery">Gallery</NavLink></li></Button>
+                                <Button></Button><li><NavLink to="/Contact">Contact</NavLink></li></Button>
+                            </Stack>
                             </ul>
                         </div>
                         <div className="content">
                             <Routes>
                                 <Route exact path="/" element={<HomePage />} />
-                                {/* <Route exact path="/packages" element={<HomePage />} />
-                                <Route exact path="/gallery" element={<HomePage />} />
-                                <Route exact path="/contact" element={<HomePage />} />
-                                 Links to more navs here  */}
+                                <Route exact path="/Packages" element={<PackagesPage />} />
+                                <Route exact path="/Gallery" element={<GalleryPage />} />
+                                <Route exact path="/Contact" element={<ContactPage />} />
                             </Routes>
                         </div>
                     </div>
